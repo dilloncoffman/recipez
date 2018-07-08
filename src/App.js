18f2755a9429286a0cb70860309aa4f248ec06fb
Dillon Coffman <dillonscoffman@gmail.com>
2018-07-08 18:17:16 -0400

    Added Recipes component, display search results to screen, styled search results on screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
+import Recipes from './components/Recipes';
 
 const API_KEY = "Your api key :)"; // Change this to your API key from food2fork.com/about/api
 
 class App extends Component {
+  state = {
+    recipes: [],
+  }
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`http://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
-
+    const api_call = await fetch(`http://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
     // get data back from api and convert to json
     const data = await api_call.json();
-    console.log(data.recipes[0].recipe_id);
+    this.setState({ recipes: data.recipes});
+    console.log(this.state.recipes);
 
   }
 
@@ -23,6 +27,7 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
